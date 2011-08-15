9ef15b00c35b681a154e9c7a7f9eb7ca9eadb7a6
Samuel Hubbard <hsamuel82@gmail.com>
2011-08-14 21:09:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FullSocialNetwork::Application.config.secret_token = 'f5daf946084dfa090d373d3333148775707114bf090c8e0a877a0f67462c8d5d4712f342c0cb95e288389c5050069de1b3163e0349f9509e7525f2de78366861'
